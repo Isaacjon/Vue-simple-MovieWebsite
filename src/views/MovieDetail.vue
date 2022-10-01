730ed57097b79ebe794cd9514ef248e443ecf282
isaacjon <izzatazimov958@gmail.com>
2022-10-01 11:08:30 +0500

    details page

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,10 +1,60 @@
 <template>
-  <div class="movie-detail">Detail {{ $route.params.id }}</div>
+  <div class="movie-detail">
+    <h2>{{ movie.Title }}</h2>
+    <p>{{ movie.Year }}</p>
+    <img :src="movie.Poster" alt="movie poster" class="featured-img" />
+    <p>{{ movie.Plot }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import { ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import env from "@/env.js";
+export default {
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
 
-<style>
+    onBeforeMount(() => {
+      console.log("beforemount ", route);
+      fetch(
+        `http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          movie.value = data;
+        });
+    });
+
+    return {
+      movie,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.movie-detail {
+  padding: 16px;
+
+  h2 {
+    color: #fff;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+
+  .featured-img {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 16px;
+  }
+
+  p {
+    color: #fff;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+}
 </style>
